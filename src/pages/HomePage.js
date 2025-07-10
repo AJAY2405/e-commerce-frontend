85a19ad5a4fffc062b202a4d842acc1c5205c09f
AJAY2405 <rahul372602@gmail.com>
2025-07-10 14:49:15 +0530

    Updated currency symbol in product price display

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -172,7 +172,7 @@ const HomePage = () => {
                     <h5 className="card-title card-price">
                       {p.price.toLocaleString("en-IN", {
                         style: "currency",
-                        currency: "USD",
+                        currency: "INR",
                       })}
                     </h5>
                   </div>
